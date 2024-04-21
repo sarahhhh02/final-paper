0b17b77d9c4a1d793e7060a5b1e7113567e27fac
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-04-21 04:06:11 -0400

    update

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,74 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests for cleaned data
+# Author: Sarah Lee
+# Date: 18 April 2024
+# Contact: sarahhhh.lee@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: cleaned data from the raw data
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(arrow)
+library(testthat)
+
+cleaned_total <- read_parquet(here::here("data/analysis_data/cleaned_total.parquet"))
+cleaned_sex <- read_parquet(here::here("data/analysis_data/cleaned_sex.parquet"))
+cleaned_age <- read_parquet(here::here("data/analysis_data/cleaned_age.parquet"))
+cleaned_sex_race <- read_parquet(here::here("data/analysis_data/cleaned_sex_race.parquet"))
+cleaned_age_sex <- read_parquet(here::here("data/analysis_data/cleaned_age_sex.parquet"))
+cleaned_sex_age_race <- read_parquet(here::here("data/analysis_data/cleaned_sex_age_race.parquet"))
+
 
 #### Test data ####
+#cleaned_total
+is.character(cleaned_sex$UNIT)
+is.numeric(cleaned_sex$YEAR)
+is.numeric(cleaned_sex$ESTIMATE)
+
+#cleaned_sex
+is.character(cleaned_sex$UNIT)
+is.numeric(cleaned_sex$YEAR)
+is.character(cleaned_sex$SEX)
+is.numeric(cleaned_sex$ESTIMATE)
+
+#cleaned_age
+# Check if variables are numeric or character
+variable <- names(cleaned_age)
+variable_types <- sapply(cleaned_age, class)
+
+# Print the variable names and their types
+for(i in 1:length(cleaned_age)) {
+  cat("variable:", variable[i], "\t Type:", variable_types[i], "\n")
+}
+
+#cleaned_sex_race
+# Check if variables are numeric or character
+variable <- names(cleaned_sex_race)
+variable_types <- sapply(cleaned_sex_race, class)
+
+# Print the variable names and their types
+for(i in 1:length(cleaned_sex_race)) {
+  cat("variable:", variable[i], "\t Type:", variable_types[i], "\n")
+}
+
+#cleaned_age_sex
+# Check if variables are numeric or character
+variable <- names(cleaned_age_sex)
+variable_types <- sapply(cleaned_age_sex, class)
+
+# Print the variable names and their types
+for(i in 1:length(cleaned_age_sex)) {
+  cat("variable:", variable[i], "\t Type:", variable_types[i], "\n")
+}
+
+#cleaned_sex_age_race
+# Check if variables are numeric or character
+variable <- names(cleaned_sex_age_race)
+variable_types <- sapply(cleaned_sex_age_race, class)
+
+# Print the variable names and their types
+for(i in 1:length(cleaned_sex_age_race)) {
+  cat("variable:", variable[i], "\t Type:", variable_types[i], "\n")
+}
+
